6038018d90eb767a2f1a90c4d6a6917e98996c52
pettekaterina <78584406+pettekaterina@users.noreply.github.com>
2021-02-05 09:44:26 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+  const flowerSwiper = new Swiper('.flowers-slider', {
+  // Параметры слайдера
+   loop: true,
+   slidesPerView: 6,
+
+  // Подключаем стрелки
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+   // Настройки адаптивной версии
+  breakpoints: {
+    // если браузер >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // если браузер >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // если браузер >= 992px
+    992: {
+      slidesPerView: 6,
+      spaceBetween: 40
+    }
+  }
+});
+
+const reviewsSwiper = new Swiper('.reviews-slider', {
+  // Параметры слайдера
+   loop: true,
+   slidesPerView: 1,
+
+  // Подключаем стрелки
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+$("#review-1").on('click', function() {
+
+  $.fancybox.open([
+    {
+      src  : 'https://source.unsplash.com/IvfoDk30JnI/1500x1000',
+      opts : {
+        caption : 'First caption',
+        thumb   : 'https://source.unsplash.com/IvfoDk30JnI/240x160'
+      }
+    },
+    {
+      src  : 'https://source.unsplash.com/0JYgd2QuMfw/1500x1000',
+      opts : {
+        caption : 'Second caption',
+        thumb   : 'https://source.unsplash.com/0JYgd2QuMfw/240x160'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+});
+
+});
